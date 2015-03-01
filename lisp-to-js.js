6ecdf5aa09267f0c23591dec5060549730910eb7
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-03-01 14:41:19 +0000

    Swap lisp-to-json with lisp-to-array

--- lisp-to-js.js
+++ lisp-to-js.js
@@ -1,16 +1,16 @@
 /* jshint node: true */
 
-var lispToJson = require("lisp-to-json"),
+var lispToArray = require("lisp-to-array"),
     lispArrayToJs = require("lisp-array-to-js");
 
 function transpileProgram(val) {
-    return lispArrayToJs(lispToJson(String(val)));
+    return lispArrayToJs(lispToArray(String(val)));
 }
 
 module.exports = transpileProgram;
 transpileProgram.transpile = function(val) {
-    return lispArrayToJs.transpile(lispToJson(String(val)));
+    return lispArrayToJs.transpile(lispToArray(String(val)));
 };
 transpileProgram.exec = function(val) {
-    return lispArrayToJs.exec(lispToJson(String(val)));
+    return lispArrayToJs.exec(lispToArray(String(val)));
 };
