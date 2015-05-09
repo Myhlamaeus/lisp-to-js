4bf9e87edc6ce21c42e2912e7cf7650d027b8105
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-05-09 18:56:55 +0000

    Use ES6, standard and gulp

--- cli.js
+++ cli.js
@@ -1,44 +1,43 @@
-#!/usr/bin/env node
-/* jshint node: true */
+#!/usr/bin/env babel-node
 
-var cli = require("cli"),
-    lispToJs = require("./"),
-    fs = require("fs");
+import cli from 'cli'
+import lispToJs from './'
+import fs from 'fs'
 
 cli.parse({
-    output: ["o", "File to save output to", "file"],
-    exec: ["e", "Execute"]
-});
+  output: ['o', 'File to save output to', 'file'],
+  exec: ['e', 'Execute']
+})
 
-cli.main(function(args, options) {
-    if(args.length !== 1) {
-        cli.error("Exactly 1 input file must be supplied");
-        cli.getUsage();
+cli.main(function (args, options) {
+  if (args.length !== 1) {
+    cli.error('Exactly 1 input file must be supplied')
+    cli.getUsage()
+  }
+  fs.readFile(args[0], function (err, content) {
+    var out
+
+    if (err) {
+      console.log(err)
+      cli.fatal(`An error occured while reading "${args[0]}"`)
     }
-    fs.readFile(args[0], function(err, content) {
-        var out;
 
-        if(err) {
-            console.log(err);
-            cli.fatal("An error occured while reading '" + args[0] + "'");
-        }
+    content = String(content)
+    if (options.exec) {
+      out = JSON.stringify(lispToJs.exec(content))
+    } else {
+      out = lispToJs(content)
+    }
 
-        content = String(content);
-        if(options.exec) {
-            out = JSON.stringify(lispToJs.exec(content));
-        } else {
-            out = lispToJs(content);
+    if (options.output) {
+      fs.writeFile(options.output, out, function (err) {
+        if (err) {
+          console.log(err)
+          cli.fatal(`An error occured while writing "${options.output}"`)
         }
-
-        if(options.output) {
-            fs.writeFile(options.output, out, function(err) {
-                if(err) {
-                    console.log(err);
-                    cli.fatal("An error occured while writing '" + options.output + "'");
-                }
-            });
-        } else {
-            console.log(out);
-        }
-    });
-});
+      })
+    } else {
+      console.log(out)
+    }
+  })
+})
--- lisp-to-js.js
+++ lisp-to-js.js
@@ -1,16 +1,14 @@
-/* jshint node: true */
+import lispToArray from 'lisp-to-array'
+import lispArrayToJs from 'lisp-array-to-js'
 
-var lispToArray = require("lisp-to-array"),
-    lispArrayToJs = require("lisp-array-to-js");
-
-function transpileProgram(val) {
-    return lispArrayToJs(lispToArray(String(val)));
+export default function (val) {
+  return lispArrayToJs(lispToArray(String(val)))
 }
 
-module.exports = transpileProgram;
-transpileProgram.transpile = function(val) {
-    return lispArrayToJs.transpile(lispToArray(String(val)));
-};
-transpileProgram.exec = function(val) {
-    return lispArrayToJs.exec(lispToArray(String(val)));
-};
+export function transpile (val) {
+  return lispArrayToJs.transpile(lispToArray(String(val)))
+}
+
+export function exec (val) {
+  return lispArrayToJs.exec(lispToArray(String(val)))
+}
